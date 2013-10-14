832c18123973471099e5873ad0eda4e363add7a4
Frank Laub <frank.laub@gmail.com>
2013-10-13 17:42:22 -0700

    Fixing rendezvous migrations

--- src/h0tb0x/db/rendezvous.go
+++ src/h0tb0x/db/rendezvous.go
@@ -109,11 +109,11 @@ CREATE INDEX IF NOT EXISTS IDX_Object_topic ON Object (topic, type, key, priorit
 			`
 PRAGMA user_version = 1;
 
-DROP TABLE IF NOT EXISTS Object;
-DROP TABLE IF NOT EXISTS Friend;
-DROP TABLE IF NOT EXISTS TopicFriend;
-DROP TABLE IF NOT EXISTS Blob;
-DROP TABLE IF NOT EXISTS Advert;
+DROP TABLE Object;
+DROP TABLE Friend;
+DROP TABLE TopicFriend;
+DROP TABLE Blob;
+DROP TABLE Advert;
 
 CREATE UNIQUE INDEX IDX_Rendezvous_fp ON Rendezvous (fingerprint);
 `,
